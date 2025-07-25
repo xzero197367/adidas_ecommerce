7474e3a2e9f830bd9622b512d6907795ada84ad5
Ahmed Khiri <Ahmed Khiri@DESKTOP-PLKQVEL>
2025-07-25 21:51:47 +0300

    task 1 repos and their interfaces

---
+++ Adidas.Application/Contracts/RepositoriesContracts/Separator/IBrandRepository.cs
@@ -0,0 +1,15 @@
+﻿using Adidas.Models.Separator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.Application.Contracts.RepositoriesContracts.Separator
+{
+    public interface IBrandRepository : IGenericRepository<Brand>
+    {
+        Task<Brand?> GetBrandByNameAsync(string name);
+        Task<IEnumerable<Brand>> GetPopularBrandsAsync();
+    }
+}
