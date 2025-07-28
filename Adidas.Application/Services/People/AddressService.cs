b5b499ec1bbea7eb68526f28540fdfc852f15ad0
Ezz-Abdelmoez <81043088+xzero197367@users.noreply.github.com>
2025-07-28 11:21:44 +0300

    before update from main branch

---
+++ Adidas.Application/Services/People/AddressService.cs
@@ -0,0 +1,50 @@
+﻿using Adidas.Application.Contracts.RepositoriesContracts;
+using Adidas.Application.Contracts.ServicesContracts.People;
+using Adidas.DTOs.People.Address_DTOs;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Models.People;
+
+namespace Adidas.Application.Services.People;
+
+public class AddressService : GenericService<Address, AddressDto, CreateAddressDto, UpdateAddressDto>, IAddressService
+{
+    public AddressService(IGenericRepository<Address> repository, IMapper mapper, ILogger logger) : base(repository,
+        mapper, logger)
+    {
+    }
+
+    public async Task<IEnumerable<AddressDto>> GetAddressesByUserIdAsync(string userId)
+    {
+        var addresses = await _repository
+            .GetAllAsync(a => a.UserId == userId && !a.IsDeleted, a => a.User);
+
+        return _mapper.Map<IEnumerable<AddressDto>>(addresses);
+    }
+
+    public async Task<AddressDto?> GetDefaultAddressAsync(string userId)
+    {
+        var defaultAddress = await 
+            _repository.FirstOrDefaultAsync(a => a.IsDefault && a.UserId == userId && !a.IsDeleted, a => a.User);
+        return _mapper.Map<AddressDto>(defaultAddress);
+    }
+
+    public async Task<AddressDto> SetDefaultAddressAsync(Guid addressId, string userId)
+    {
+        var address = await _repository.GetByIdAsync(addressId);
+        address.IsDefault = true;
+        var updatedAddress = await _repository.UpdateAsync(address);
+        // await _repository.SaveChangesAsync();
+        return _mapper.Map<AddressDto>(updatedAddress.Entity);
+    }
+
+    public Task<bool> ValidateAddressAsync(CreateAddressDto addressDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<string>> GetSuggestedAddressesAsync(string partialAddress)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Adidas.DTOs/Main/Product Variant DTOs/ProductVariantDto.cs
+++ Adidas.DTOs/Main/Product Variant DTOs/ProductVariantDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Adidas.DTOs.Common_DTOs;
 using Adidas.DTOs.Main.Product_DTOs;
 
