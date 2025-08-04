0aa91091676c75077a7828115898830d9b4ed0f4
ayaalwarrdany <attallaa937@gmail.com>
2025-08-04 07:00:02 +0300

    add ResultDTO in common DTOS folder

---
+++ Adidas.DTOs/Common DTOs/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.DTOs.Common_DTOs
+{
+    internal class Result
+    {
+    }
+}
