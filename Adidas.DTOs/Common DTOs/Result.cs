e3dc6662342f2f7e10522f136c21090f748e678b
ayaalwarrdany <attallaa937@gmail.com>
2025-08-04 07:02:29 +0300

    edit create category in the service file

--- Adidas.DTOs/Common DTOs/Result.cs
+++ Adidas.DTOs/Common DTOs/Result.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Adidas.DTOs.Common_DTOs
 {
-    internal class Result
+    public class Result
     {
+        public bool IsSuccess { get; }
+        public string Error { get; }
+
+        private Result(bool isSuccess, string error) => (IsSuccess, Error) = (isSuccess, error);
+
+        public static Result Success() => new(true, null);
+        public static Result Failure(string error) => new(false, error);
     }
+
 }
