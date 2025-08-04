0a3b0265501a6c662e6ae9c17e6a9d2c107ecd0a
ayaalwarrdany <attallaa937@gmail.com>
2025-08-04 08:14:51 +0300

    add edit category view

--- Adidas.Application/Contracts/ServicesContracts/Separator/ICategoryService.cs
+++ Adidas.Application/Contracts/ServicesContracts/Separator/ICategoryService.cs
@@ -15,6 +15,7 @@ namespace Adidas.Application.Contracts.ServicesContracts.Separator
         Task<IEnumerable<CategoryDto>> GetMainCategoriesAsync();
         Task<Result> CreateAsync(CreateCategoryDto createCategoryDto);
         Task<Result>DeleteAsync(Guid id);
+        Task<Result> UpdateAsync(UpdateCategoryDto updateCategoryDto);
         //Task<IEnumerable<CategoryDto>> GetSubCategoriesAsync(Guid parentCategoryId);
         //Task<CategoryResponseDto?> GetCategoryBySlugAsync(string slug);
         //Task<List<CategoryHierarchyDto>> GetCategoryHierarchyAsync(Guid categoryId);
