a6392f36dedc495adc7370468fe432690b58d360
MOSTAFA17RASHEEDY <mostafarasheedy17@gmail.com>
2025-08-10 21:14:48 +0300

    Add Inventory Controller

---
+++ Adidas.AdminDashboardMVC/ViewModels/Inventory/InventoryDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Adidas.DTOs.Tracker;
+
+namespace Adidas.AdminDashboardMVC.ViewModels.Inventory
+{
+    public class InventoryDashboardViewModel
+    {
+        public InventoryReportDto Report { get; set; } = new InventoryReportDto
+        {
+            TotalProducts = 0,
+            TotalVariants = 0,
+            LowStockVariants = 0,
+            OutOfStockVariants = 0,
+            TotalInventoryValue = 0,
+            ProductStocks = new List<ProductStockDto>()
+        };
+
+        public IEnumerable<LowStockAlertDto> LowStockAlerts { get; set; } = new List<LowStockAlertDto>();
+    }
+}
