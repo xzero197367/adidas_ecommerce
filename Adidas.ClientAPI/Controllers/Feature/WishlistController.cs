2237f91b0ad2c522121a4afe3ebe3ecf882126a9
Ahmed Khiri <Ahmed Khiri@DESKTOP-PLKQVEL>
2025-08-20 00:52:14 +0300

    kkmk

---
+++ Adidas.ClientAPI/Controllers/Feature/WishlistController.cs
@@ -0,0 +1,29 @@
+﻿using Adidas.Application.Contracts.ServicesContracts.Feature;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adidas.ClientAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WishlistController : ControllerBase
+    {
+        private readonly IWishListService _wishlistService;
+
+        public WishlistController(IWishListService wishlistService)
+        {
+            _wishlistService = wishlistService;
+        }
+
+        /// <summary>
+        /// Get all products in a user's wishlist
+        /// </summary>
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetAllProductsInWishlist(string userId)
+        {
+            var result = await _wishlistService.GetWishlistByUserIdAsync(userId);
+            if (!result.IsSuccess) return BadRequest(result.ErrorMessage);
+
+            return Ok(result.Data);
+        }
+    }
+}
