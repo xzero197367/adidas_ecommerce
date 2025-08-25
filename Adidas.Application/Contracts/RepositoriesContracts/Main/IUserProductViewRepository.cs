cfe1da7dcfc6b82580e9e7a88b380f7c7fdbbc42
ayaalwarrdany <attallaa937@gmail.com>
2025-08-25 12:35:50 +0300

    add IuserProductViewRepo and its implementation

---
+++ Adidas.Application/Contracts/RepositoriesContracts/Main/IUserProductViewRepository.cs
@@ -0,0 +1,16 @@
+﻿using Models.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.Application.Contracts.RepositoriesContracts.Main
+{
+    public interface IUserProductViewRepository
+    {
+        Task AddAsync(UserProductView view);
+        Task<bool> ExistsAsync(string userId, Guid productId);
+        Task<List<UserProductView>> GetByProductIdAsync(Guid productId);
+    }
+}
---
+++ Adidas.Infra/Main/UserProductViewRepository.cs
@@ -0,0 +1,38 @@
+﻿using Adidas.Application.Contracts.RepositoriesContracts.Main;
+using Adidas.Models.Main;
+using Models.Main;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.Infra.Main
+{
+    public class UserProductViewRepository : IUserProductViewRepository
+    {
+        private readonly AdidasDbContext _context;
+        public UserProductViewRepository(AdidasDbContext context) => _context = context;
+
+        public async Task AddAsync(UserProductView view)
+        {
+            _context.UserProductViews.Add(view);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(string userId, Guid productId)
+        {
+            
+            return await _context.UserProductViews
+                                 .AnyAsync(v => v.UserId == userId && v.ProductId == productId);
+        }
+
+        public async Task<List<UserProductView>> GetByProductIdAsync(Guid productId) =>
+            await _context.UserProductViews
+                .Where(v => v.ProductId == productId)
+                .ToListAsync();
+    }
+
+
+}
