dec39b461da1289d31082bcf4bf40efd04a66926
ayaalwarrdany <attallaa937@gmail.com>
2025-08-25 12:34:07 +0300

    add UserProductViews Model

---
+++ Models/Main/UserProductViews.cs
@@ -0,0 +1,21 @@
+﻿using Adidas.Models.Main;
+using Models.People;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Main
+{
+    public class UserProductView 
+    {
+        public Guid Id { get; set; }
+        public  string UserId { get; set; }
+        public Guid ProductId { get; set; }  
+        public DateTime ViewedAt { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; }
+        public Product Product { get; set; }
+    }
+}
