d2d6a1c1dac855963cbeef5e726ed549cf365be0
ayaalwarrdany <attallaa937@gmail.com>
2025-08-25 12:36:59 +0300

    add IrecommendationService and its implementaion

---
+++ Adidas.Application/Contracts/ServicesContracts/Main/IRecommendationService.cs
@@ -0,0 +1,15 @@
+﻿using Adidas.Models.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.Application.Contracts.ServicesContracts.Main
+{
+    public interface IRecommendationService
+    {
+        Task<List<Product>> GetRecommendationsAsync(Guid productId);
+
+    }
+}
---
+++ Adidas.Application/Services/Main/RecommendationService.cs
@@ -0,0 +1,43 @@
+﻿using Adidas.Application.Contracts.RepositoriesContracts.Main;
+using Adidas.Application.Contracts.ServicesContracts.Main;
+using Adidas.Models.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adidas.Application.Services.Main
+{
+    // Application/Services/RecommendationService.cs
+    public class RecommendationService : IRecommendationService
+    {
+        private readonly IUserProductViewRepository _viewRepo;
+        private readonly IProductRepository _productRepo;
+
+        public RecommendationService(IUserProductViewRepository viewRepo, IProductRepository productRepo)
+        {
+            _viewRepo = viewRepo;
+            _productRepo = productRepo;
+        }
+
+        public async Task<List<Product>> GetRecommendationsAsync(Guid productId)
+        {
+            var views = await _viewRepo.GetByProductIdAsync(productId);
+            var userIds = views.Select(v => v.UserId).Distinct().ToList();
+
+            var otherViews = views
+                .Where(v => v.ProductId != productId && userIds.Contains(v.UserId))
+                .GroupBy(v => v.ProductId)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .Take(5)
+                .ToList();
+
+            var recommendedProducts = await _productRepo.GetByIdsAsync(otherViews);
+
+            return recommendedProducts;
+        }
+    }
+
+}
