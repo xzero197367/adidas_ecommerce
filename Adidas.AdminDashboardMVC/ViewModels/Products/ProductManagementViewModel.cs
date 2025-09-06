dd6408924747516f030735e8783c99d45a222f3e
ayaalwarrdany <attallaa937@gmail.com>
2025-09-06 17:52:53 +0300

    feat(viewmodel): add ProductManagementViewModel for product filtering and pagination

---
+++ Adidas.AdminDashboardMVC/ViewModels/Products/ProductManagementViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Adidas.DTOs.Common_DTOs;
+using Adidas.DTOs.Main.Product_DTOs;
+using Adidas.DTOs.Main.ProductDTOs;
+
+namespace Adidas.AdminDashboardMVC.ViewModels.Products
+{
+    public class ProductManagementViewModel
+    {
+        public ProductFilterDto Filters { get; set; } = new();
+        public PagedResultDto<ProductDto> Products { get; set; } = new();
+    }
+}
